77ed7713007706327a0da992a083da068f617cc7
Muhammad Tehami <151735000+tehami326@users.noreply.github.com>
2026-08-07 19:48:18 +0530

    Update hero image URL in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const heroImg = "https://images.unsplash.com/photo-1616486338812-3dadae4b4ace?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG9tZSUyMGRlY29yfGVufDB8fDB8fHww";
+const heroImg = "https://plus.unsplash.com/premium_photo-1664202526559-e21e9c0fb46a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2xvdGhlc3xlbnwwfHwwfHx8MA%3D%3D";
 
 const categories = [
   { name: 'Garments', href: '/garments' },         
